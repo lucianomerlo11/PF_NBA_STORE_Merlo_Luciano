ae266b131ff02305dd4dde03ad8ec470f4c910e5
lucianomerlo11 <merlo1411@gmail.com>
2022-04-09 20:40:27 -0300

    Mejoro la interfaz de DetailItemContainer

--- src/components/ItemDetailComponent/ItemDetail.js
+++ src/components/ItemDetailComponent/ItemDetail.js
@@ -1,16 +1,50 @@
 import bootstrap from "bootstrap";
 import "./ItemDetail.css"
-const ItemDetail = ({id, name, image, category, description, price, stock}) => {
+const ItemDetail = ({id, name, image, category, description, price, stock, size}) => {
+
+
+
     return (
-        <div key={id} className="card">
-            <img src={image} className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <h2 className="card-title">{name}</h2>
-                    <h6 className="card-subtitle mb-2 text-muted">{category}</h6>
-                    <p className="card-text">{description}</p>
-                    <p className="card-text">{stock}</p>
+
+        <div className="container-fluid mt-5">
+            <div className="row">
+                <div className="col-md-7">
+                    <img src={image} className="img-fluid imgProduct" alt="..." />
                 </div>
+                <div className="col-md-5">
+                    <h3>{name}</h3>
+                    <div className="sizeContainer">
+                        <h6>Talles</h6>
+                        <div className="talles">
+                            {size.map(talle => 
+                                <div className="talle me-1">
+                                    <a className="itemTalle">
+                                        {talle}
+                                    </a>
+                                </div>
+                            )}
+                            <div className="buy-stock col-md-12 mt-5">
+                                <div className="col-md-3">
+                                    <p><b>Stock:</b> {stock}</p>
+                                </div>
+                                <div className="col-md-9">
+                                    <button type='button' className="btn btn-outline-success">Comprar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        // <div key={id} className="card">
+        //     <img src={image} className="card-img-top" alt="..."/>
+        //         <div className="card-body">
+        //             <h2 className="card-title">{name}</h2>
+        //             <h6 className="card-subtitle mb-2 text-muted">{category}</h6>
+        //             <p className="card-text">{description}</p>
+        //             <p className="card-text">{stock}</p>
+        //         </div>
+        // </div>
     )
 }
 
